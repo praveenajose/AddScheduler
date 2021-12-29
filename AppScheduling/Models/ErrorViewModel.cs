6f4442fa30c7461c6124520336fb975a60825506
praveena <ppj.pravyna@gmail.com>
2021-12-29 14:17:33 +0000

    Added Project AppScheduler. ApplicationDbContext created. Identity table creted

---
+++ AppScheduling/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AppScheduling.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ AppScheduling/Models/ViewModels/LoginVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppScheduling.Models.ViewModels
+{
+    public class LoginVM
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name ="Rmemeber Me?")]
+        public bool RememberMe { get; set; }
+    }
+}
