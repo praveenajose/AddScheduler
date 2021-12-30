75f205c5c6765bdcd9b0e925201a339b5b02c8dd
praveena <ppj.pravyna@gmail.com>
2021-12-30 15:13:44 +0000

    Added Login view and Register view model

--- AppScheduling/Controllers/AccountController.cs
+++ AppScheduling/Controllers/AccountController.cs
@@ -16,7 +16,7 @@ namespace AppScheduling.Controllers
             _db = db;
         }
 
-        public IActionResult Index()
+        public IActionResult Login()
         {
             return View();
         }
---
+++ AppScheduling/Models/ViewModels/RegisterVM.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppScheduling.Models.ViewModels
+{
+    public class RegisterVM
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(15, ErrorMessage ="The {0} must be at least {6} characters long.", MinimumLength =6)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name ="Confirm Password")]
+        [Compare("Password", ErrorMessage ="The Password doesn't match")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "Role Name")]
+        public string RoleName { get; set; }
+    }
+}
